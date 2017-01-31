886ef2993c6b960971751f638e0d810acfb3edf8
Luken1990 <l.nguyen_1990@yahoo.com>
2017-01-31 20:43:07 +0000

    About section added

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
-	$(".gallery-nav li a").on("click", function(){
-		$(".gallery-nav li.active").removeClass("active");
+	$("ul.nav li a").on("click", function(){
+		$("ul.nav li.active").removeClass("active");
 		$(this).parent().addClass("active");
 		
 		var category = $(this).text().toLowerCase().replace(" ", "-");
 		
 		if(category === "all"){
 			$("#main .container .row div").fadeIn("slow").removeClass("hidden");
-		}else{
+			$("#about").addClass("hidden");
+			$("#main").removeClass("hidden");
+		}
+		else if(category === "about"){
+			$("#about").removeClass("hidden");
+			$("#main").addClass("hidden");
+			$("#about .container .row .content-wrapper h2").addClass("animated fadeInDown");
+		}
+		else{
 			$("#main .container .row div").each(function(){
 				if(!$(this).hasClass(category)){
 					$(this).hide().addClass("hidden");
+					$("#main").removeClass("hidden");
+					$("#about").addClass("hidden");
 				}else{
 					$(this).fadeIn("slow").removeClass("hidden");
+					$("#about").addClass("hidden");
 				}
 			});
 		}
@@ -22,5 +33,6 @@ $(document).ready(function(){
 	$(".navbar-toggle").on("click", function(){
 		$(this).toggleClass("open");
 	});
+	
 	
 });
